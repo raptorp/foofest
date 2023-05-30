054492b6e7c4ee2dbd620da24bbadfcbd7e2b3f1
Sabrina S <70270413+raptorp@users.noreply.github.com>
2023-05-30 23:25:51 +0200

    Added styling for camp options page

--- not-server/foofest/src/pages/buyingStage/campingAddOns/index.js
+++ not-server/foofest/src/pages/buyingStage/campingAddOns/index.js
@@ -1,322 +1,338 @@
-import React from 'react'
-import { useContext, useState, useEffect } from 'react'
-import TicketsContext from "../../../context/ticketsContext"
-import styles from "./campingAddOns.module.css"
-import ThirdTitle from "../../../components/ThirdTitle/ThirdTitle"
-import Link from 'next/link'
-import Button from "../../../components/button/Button"
+import React from "react";
+import { useContext, useState, useEffect } from "react";
+import TicketsContext from "../../../context/ticketsContext";
+import law from "./campingAddOns.module.css";
+import ThirdTitle from "../../../components/ThirdTitle/ThirdTitle";
+import Link from "next/link";
+import Button from "../../../components/button/Button";
+import Image from "next/image";
 
+import svartheimImg from "../../../public/imgs/campImgs/svartheim.jpg";
 
+import {
+  RxPlusCircled,
+  RxMinusCircled,
+  RxQuestionMarkCircled,
+} from "react-icons/rx";
+
+import {
+  Bs1CircleFill,
+  Bs1Circle,
+  Bs2CircleFill,
+  Bs2Circle,
+  Bs3CircleFill,
+  Bs3Circle,
+  Bs4CircleFill,
+  Bs4Circle,
+  Bs5CircleFill,
+  Bs5Circle,
+  BsCheckCircleFill,
+  BsCheckCircle,
+} from "react-icons/bs";
+
+import BuyFlowLayout from "../../../components/BuyFlowLayout/BuyFlowLayout";
 
 function campingAddOns() {
+  // bring context to this page
 
-
-
-// bring context to this page
-
-const globalMoneyContext = useContext(TicketsContext);
+  const globalMoneyContext = useContext(TicketsContext);
 
   // store href for "back" btn at the end
   const goBackBtnHref = "../../camping/" + globalMoneyContext.selectedCamp;
 
+  // this is the cost of tickets from the previous page
 
-// this is the cost of tickets from the previous page
+  const theTicketsCost = globalMoneyContext.costOfTickets;
 
-const theTicketsCost = globalMoneyContext.costOfTickets;
+  // this is the cost of camping
 
-// this is the cost of camping
+  const [camping, setCamping] = useState(0);
 
-const [camping, setCamping] = useState(0);
+  // this is the cost of tickets plus tents
 
-// this is the cost of tickets plus tents 
+  function addTentCostToTickets() {
+    globalMoneyContext.setTicketsPlusTents(
+      (theTicketsCost) => theTicketsCost + camping + vat
+    );
+  }
 
-function addTentCostToTickets(){
-  globalMoneyContext.setTicketsPlusTents(theTicketsCost => theTicketsCost + camping + vat );
-}
+  // store num of personal tents as state
 
+  const [personalTents, setPersonalTents] = useState(0);
 
-// store num of personal tents as state 
+  // onClick functions for increasing and reducing the num of personal tents, max 4
 
-const [personalTents, setPersonalTents] = useState(0);
+  function addPersonalTent() {
+    if (personalTents < 4) {
+      setPersonalTents((old) => old + 1);
+      setNumOfTents((old) => old + 1);
+    }
+  }
 
-// onClick functions for increasing and reducing the num of personal tents, max 4
+  function subtractPersonalTent() {
+    if (personalTents > 0) {
+      setPersonalTents((old) => old - 1);
+      setNumOfTents((old) => old - 1);
+    }
+  }
 
-function addPersonalTent(){
-  if(personalTents < 4){
-  setPersonalTents(old => old+1);
-  setNumOfTents(old => old+1)
+  // store num of EcoSwamp tents as state
 
-}
-}
+  const [ecoSwampTents, setEcoSwampTents] = useState(0);
 
-function subtractPersonalTent(){
-  if(personalTents > 0){
-  setPersonalTents(old => old-1);
-  setNumOfTents(old => old-1)
+  // onClick functions for increasing and reducing the num of EcoSwamp tents, max 4
 
-}
-}
+  function addEcoSwamp() {
+    if (ecoSwampTents < 4) {
+      setEcoSwampTents((old) => old + 1);
+      setNumOfTents((old) => old + 1);
+    }
+  }
 
-  // store num of EcoSwamp tents as state 
+  function subtractEcoSwamp() {
+    if (ecoSwampTents > 0) {
+      setEcoSwampTents((old) => old - 1);
+      setNumOfTents((old) => old - 1);
+    }
+  }
 
+  // store num of SwampLux for 2 people as state
 
-const [ecoSwampTents, setEcoSwampTents] = useState(0);
+  const [swampLuxForTwo, setSwampLuxForTwo] = useState(0);
 
-// onClick functions for increasing and reducing the num of EcoSwamp tents, max 4
+  // onClick functions for increasing and reducing the num of EcoSwamp tents, max 4
 
-function addEcoSwamp(){
-  if(ecoSwampTents < 4){
-  setEcoSwampTents(old => old+1);
-  setNumOfTents(old => old+1)
+  function addSwampLuxForTwo() {
+    if (swampLuxForTwo < 4) {
+      setSwampLuxForTwo((old) => old + 1);
+      setNumOfTents((old) => old + 1);
+    }
+  }
 
-}
-}
+  function subtractSwampLuxForTwo() {
+    if (swampLuxForTwo > 0) {
+      setSwampLuxForTwo((old) => old - 1);
+      setNumOfTents((old) => old - 1);
+    }
+  }
 
-function subtractEcoSwamp(){
-  if(ecoSwampTents > 0){
-  setEcoSwampTents(old => old-1);
-  setNumOfTents(old => old-1)
+  // store num of SwampLux for 3 people as state
 
-}
-}
+  const [swampLuxForThree, setSwampLuxForThree] = useState(0);
 
- // store num of SwampLux for 2 people as state 
+  // onClick functions for increasing and reducing the num of EcoSwamp tents, max 4
 
+  function addSwampLuxForThree() {
+    if (swampLuxForThree < 4) {
+      setSwampLuxForThree((old) => old + 1);
+      setNumOfTents((old) => old + 1);
+    }
+  }
 
- const [swampLuxForTwo, setSwampLuxForTwo] = useState(0);
+  function subtractSwampLuxForThree() {
+    if (swampLuxForThree > 0) {
+      setSwampLuxForThree((old) => old - 1);
+      setNumOfTents((old) => old - 1);
+    }
+  }
 
- // onClick functions for increasing and reducing the num of EcoSwamp tents, max 4
- 
- function addSwampLuxForTwo(){
-   if(swampLuxForTwo < 4){
-   setSwampLuxForTwo(old => old+1);
-   setNumOfTents(old => old+1)
+  // store num of tents selected, despite which ones
 
- }
- }
- 
- function subtractSwampLuxForTwo(){
-   if(swampLuxForTwo > 0){
-   setSwampLuxForTwo(old => old-1);
-   setNumOfTents(old => old-1)
+  const [numOfTents, setNumOfTents] = useState(0);
 
- }
- }
+  // store cost of tent vat
 
- // store num of SwampLux for 3 people as state 
+  const [vat, setVat] = useState(0);
 
+  // store total in this page, to later add it as a global value
 
- const [swampLuxForThree, setSwampLuxForThree] = useState(0);
+  const [campingAllInclusive, setCampingAllInclusive] = useState(0);
 
- // onClick functions for increasing and reducing the num of EcoSwamp tents, max 4
- 
- function addSwampLuxForThree(){
-   if(swampLuxForThree < 4){
-   setSwampLuxForThree(old => old+1);
-   setNumOfTents(old => old+1)
- }
- }
- 
- function subtractSwampLuxForThree(){
-   if(swampLuxForThree > 0){
-   setSwampLuxForThree(old => old-1);
-   setNumOfTents(old => old-1)
+  useEffect(() => {
+    setCampingAllInclusive(camping + vat + theTicketsCost);
+    setCamping(
+      swampLuxForThree * 399 + swampLuxForTwo * 299 + ecoSwampTents * 249
+    );
+    setVat(Math.floor(camping / 25));
+    globalMoneyContext.setTicketsPlusTents(campingAllInclusive);
+    globalMoneyContext.setTotalCampingCost(camping);
+    globalMoneyContext.setGlobalVat((oldVat) => oldVat + vat);
+  }, [vat, camping, swampLuxForThree, swampLuxForTwo, ecoSwampTents]);
 
- }
- }
+  return (
+    <BuyFlowLayout>
+      <div className={law.contentAndBasket}>
+        <div className={law.content}>
+          <div className={law.imageContainer}>
+            <Image
+              className={law.imgStyle}
+              src={svartheimImg}
+              sizes="(max-width: 1920px) 100vw, 750px"
+              alt="/"
+            />
+          </div>
+          <div className={law.campInfo}>
+            <ThirdTitle thirdTitle="Camping add-ons" />
+            <p className={law.description}>
+              Esse minim dolor nostrud Lorem proident cupidatat aliqua proident
+              sunt minim ut amet aliquip ex.
+            </p>
+          </div>
+          <div className={law.addContainer}>
+            <h3 className={law.addTitle}>How would you like to camp?</h3>
 
- // store num of tents selected, despite which ones 
+            <div className={law.typeAndButtons}>
+              <div className={law.typeText}>
+                <p>
+                  Use your own tent
+                  <RxQuestionMarkCircled size={16} />
+                </p>
+                <span>Free</span>
+              </div>
+              <div className={law.typeButtons}>
+                <RxMinusCircled
+                  size={32}
+                  onClick={subtractPersonalTent}
+                  className={law.selectIcons}
+                />
+                <span>{personalTents}</span>
+                <RxPlusCircled
+                  size={32}
+                  onClick={addPersonalTent}
+                  className={law.selectIcons}
+                />
+              </div>
+            </div>
 
- const [numOfTents, setNumOfTents] = useState(0);
+            <div className={law.typeAndButtons}>
+              <div className={law.typeText}>
+                <p>
+                  EcoSwamp Camping
+                  <RxQuestionMarkCircled size={16} />
+                </p>
+                <span>+249.00 kr. pr tent</span>
+              </div>
+              <div className={law.typeButtons}>
+                <RxMinusCircled
+                  size={32}
+                  onClick={subtractEcoSwamp}
+                  className={law.selectIcons}
+                />
+                <span>{ecoSwampTents}</span>
+                <RxPlusCircled
+                  size={32}
+                  onClick={addEcoSwamp}
+                  className={law.selectIcons}
+                />
+              </div>
+            </div>
 
+            <div className={law.typeAndButtons}>
+              <div className={law.typeText}>
+                <p>
+                  SwampLux for 2 people
+                  <RxQuestionMarkCircled size={16} />
+                </p>
+                <span>+299.00 kr. per tent</span>
+              </div>
+              <div className={law.typeButtons}>
+                <RxMinusCircled
+                  size={32}
+                  onClick={subtractSwampLuxForTwo}
+                  className={law.selectIcons}
+                />
+                <span>{swampLuxForTwo}</span>
+                <RxPlusCircled
+                  size={32}
+                  onClick={addSwampLuxForTwo}
+                  className={law.selectIcons}
+                />
+              </div>
+            </div>
 
+            <div className={law.typeAndButtons}>
+              <div className={law.typeText}>
+                <p>
+                  SwampLux for 3 people
+                  <RxQuestionMarkCircled size={16} />
+                </p>
+                <span>+399.00 kr. per tent</span>
+              </div>
+              <div className={law.typeButtons}>
+                <RxMinusCircled
+                  size={32}
+                  onClick={subtractSwampLuxForThree}
+                  className={law.selectIcons}
+                />
+                <span>{swampLuxForThree}</span>
+                <RxPlusCircled
+                  size={32}
+                  onClick={addSwampLuxForThree}
+                  className={law.selectIcons}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
 
+        <div className={law.basketContainer}>
+          <h3 className={law.orderTitle}>Order Summary</h3>
 
-// store cost of tent vat
+          <div className={law.ticketAmountContainer}>
+            <span>Tickets:</span>
+            <span className={law.ticketNum}>
+              {globalMoneyContext.howManyTickets}
+            </span>
+          </div>
 
-const [vat, setVat] = useState(0);
+          <div className={law.ticketsContainer}>
+            <span>Ticket(s)</span>
+            <span className={law.ticketNum}>{theTicketsCost} kr.</span>
+          </div>
 
-// store total in this page, to later add it as a global value
+          <div className={law.campingContainer}>
+            <span>Camping</span>
+            <span className={law.ticketNum}>{camping} kr.</span>
+          </div>
 
-const [campingAllInclusive, setCampingAllInclusive] = useState(0)
+          <div className={law.vatContainer}>
+            <span>VAT</span>
+            <span className={law.vatNum}>{vat} kr.</span>
+          </div>
 
-
-useEffect(() => {
-
-  setCampingAllInclusive(camping + vat + theTicketsCost)
-  setCamping((swampLuxForThree * 399) + (swampLuxForTwo * 299) + (ecoSwampTents * 249))
-  setVat(Math.floor(camping / 25));
-  globalMoneyContext.setTicketsPlusTents(campingAllInclusive);
-  globalMoneyContext.setTotalCampingCost(camping);
-  globalMoneyContext.setGlobalVat(oldVat => oldVat + vat);
-
-}, [vat, camping, swampLuxForThree, swampLuxForTwo, ecoSwampTents])
-
-
-
-return (
-  <>
-  <main className={styles.main}>
-
-    <section className={styles.details} >
-
-      <div className={styles.nordicLights}>
-
+          <div className={law.totalContainer}>
+            <span>Total</span>
+            <span className={law.totalNum}>
+              {globalMoneyContext.ticketsPlusTents} kr.
+            </span>
+          </div>
+        </div>
       </div>
-      
-      
-      <article className={styles.orderSummary}>
-        <span className={styles.orderTitle}>Order Summary</span>
+      <div className={law.flowNav}>
+        <div className={law.backNextButtons}>
+          <Link href={goBackBtnHref}>
+            <button className={law.backButton}>GO BACK</button>
+          </Link>
 
-        <div className={styles.orderTotalDiv}>
-          <span>Tickets:</span>
-          <span className={styles.orderTicketCost}>{globalMoneyContext.howManyTickets}</span>
+          <Link href="/buyingStage/ticketHolderInfo">
+            <button
+              className={law.nextButton}
+              disabled={numOfTents == 0 ? true : false}
+            >
+              NEXT STEP
+            </button>
+          </Link>
         </div>
-
-        <div className={styles.orderTotalDiv}>
-          <span>Tickets cost</span>
-          <span className={styles.orderTicketCost}>{theTicketsCost} kr.</span>
+        <div className={law.stepNum}>
+          <BsCheckCircle className={law.checkedStep} size={24} />
+          <Bs2Circle className={law.currentStep} size={24} />
+          <Bs3Circle className={law.lowOpacity} size={24} />
+          <Bs4Circle className={law.lowOpacity} size={24} />
+          <Bs5Circle className={law.lowOpacity} size={24} />
         </div>
-
-        <div className={styles.orderTaxesDiv}>
-          <span>Camping cost</span>
-          <span className={styles.orderTaxesNum}>{camping} kr.</span>
-        </div>
-
-        <div className={styles.orderTaxesDiv}>
-          <span>VAT</span>
-          <span className={styles.orderTaxesNum}>{vat} kr.</span>
-        </div>
-
-        <div className={styles.orderTotalDiv}>
-          <span>Total</span>
-          <span className={styles.orderTotalNum}>{globalMoneyContext.ticketsPlusTents} kr.</span>
-        </div>
-      </article>
-      
-      <div className={styles.allText}>
-          <ThirdTitle
-          
-            thirdTitle="Camping"
-          />
-
-          <p className={styles.description}>Esse minim dolor nostrud Lorem proident cupidatat aliqua proident sunt minim ut amet aliquip ex. 
-             </p>
-
       </div>
-
-      <div className={styles.selectTickets}>
-
-        <span className={styles.selectTitle}><strong>How would you like to camp?</strong></span>
-
-        <div className={styles.selectRegular}>
-
-              <div className={styles.selectRegularDiv}>
-                <span><strong>Use your own tent</strong></span>
-                <span className={styles.ticketSpecs}>Free</span>
-              </div>
-
-              <div className={styles.selectRegularAmount}>
-                <span onClick={subtractPersonalTent}  className={styles.selectIcons}>-</span>
-                <span className={styles.numOfTickets}>{personalTents}</span>
-                <span onClick={addPersonalTent} className={styles.selectIcons}>+</span>
-              </div>
-
-        </div>
-        {/*------------------*/}
-
-        <div className={styles.selectVip}>
-              
-              <div className={styles.selectVipDiv}>
-                <span><strong>EcoSwamp Camping</strong></span>
-                <span className={styles.ticketSpecs}>+249.00 kr. pr tent</span>
-              </div>
-
-              <div className={styles.selectVipAmount}>
-                <span onClick={subtractEcoSwamp}  className={styles.selectIcons}>-</span>
-                <span className={styles.numOfTickets}>{ecoSwampTents}</span>
-                <span onClick={addEcoSwamp} className={styles.selectIcons}>+</span>
-              </div>
-
-        </div>
-
-          {/*------------------*/}
-
-        <div className={styles.selectRegular}>
-
-              <div className={styles.selectRegularDiv}>
-                <span><strong>SwampLux for 2 people</strong></span>
-                <span className={styles.ticketSpecs}>+299.00 kr. per tent</span>
-              </div>
-
-              <div className={styles.selectRegularAmount}>
-                <span onClick={subtractSwampLuxForTwo}  className={styles.selectIcons}>-</span>
-                <span className={styles.numOfTickets}>{swampLuxForTwo}</span>
-                <span onClick={addSwampLuxForTwo}  className={styles.selectIcons}>+</span>
-              </div>
-
-        </div>
-        {/*------------------*/}
-
-        <div className={styles.selectRegular}>
-
-              <div className={styles.selectRegularDiv}>
-                <span><strong>SwampLux for 3 people</strong></span>
-                <span className={styles.ticketSpecs}>+399.00 kr. per tent</span>
-              </div>
-
-              <div className={styles.selectRegularAmount}>
-                <span onClick={subtractSwampLuxForThree}  className={styles.selectIcons}>-</span>
-                <span className={styles.numOfTickets}>{swampLuxForThree}</span>
-                <span onClick={addSwampLuxForThree}  className={styles.selectIcons}>+</span>
-              </div>
-
-        </div>
-        {/*------------------*/}
-
-
-
-      
-      </div>
-
-      <div className={styles.darkBtns}>
-        <div className={styles.innerDivBtns}>
-
-        <Link
-        className={styles.backLink} 
-        href={goBackBtnHref}>
-
-        <Button title="BACK" 
-        />
-        
-        </Link>
-
-        <Button title="RESET STEP"
-        />
-
-        </div>
-       
-
-      </div>
-
-      <div className={styles.nextStep}>
-
-        <Link href="/buyingStage/ticketHolderInfo"
-        >
-         <button className={styles.nextStepBtn}
-          disabled={numOfTents == 0 ? true : false}
-          >
-            NEXT STEP
-          </button>
-        </Link>
-      </div>
-     
-     
-
-    </section>
-  </main>
-  </>
-)
+    </BuyFlowLayout>
+  );
 }
 
-export default campingAddOns
+export default campingAddOns;
