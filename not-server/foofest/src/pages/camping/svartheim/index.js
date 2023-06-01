4e620544be2ebaf46851e3fa5541a659330d819f
Federico Barbieri <federicobarbieri54@gmail.com>
2023-06-01 17:57:32 -0300

    light mode svarthein works

--- not-server/foofest/src/pages/camping/svartheim/index.js
+++ not-server/foofest/src/pages/camping/svartheim/index.js
@@ -273,7 +273,7 @@ function svartheim() {
         </div>
 
         <div className={law.basketContainer}>
-          <h3 className={law.orderTitle}>Order Summary | {globalMoneyContext.howManyTickets === 0 ? "" : <Timer />}</h3>
+          <h3 className={law.orderTitle}>Order Summary</h3>
           <div className={law.ticketsContainer}>
             <span>Ticket(s)</span>
             <span className={law.ticketNum}>{ticketCost} kr.</span>
